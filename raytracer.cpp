99df1fba28533a8bc72afbc6ab7e84cbf829017d
baharrsalmann <bahar.salman@metu.edu.tr>
2024-10-26 13:01:32 +0300

    first_commit

--- raytracer.cpp
+++ raytracer.cpp
@@ -1,8 +1,92 @@
 #include <iostream>
 #include "parser.h"
 #include "ppm.h"
+using namespace parser;
 
 typedef unsigned char RGB[3];
+
+Vec3f operator+(const Vec3f& a, const Vec3f& b) {
+    return {a.x + b.x, a.y + b.y, a.z + b.z};
+}
+
+Vec3f operator-(const Vec3f& a, const Vec3f& b) {
+    return {a.x - b.x, a.y - b.y, a.z - b.z};
+}
+
+Vec3f operator*(const Vec3f& v, float s) {
+    return {v.x * s, v.y * s, v.z * s};
+}
+
+float dot(const Vec3f& a, const Vec3f& b) {
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+Vec3f normalize(const Vec3f& v) {
+    float len = std::sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+    return {v.x / len, v.y / len, v.z / len};
+}
+
+Vec3f cross(const Vec3f& a, const Vec3f& b) {
+    return {
+        a.y * b.z - a.z * b.y,
+        a.z * b.x - a.x * b.z,
+        a.x * b.y - a.y * b.x
+    };
+}
+
+class Ray {
+    Vec3f origin, direction;
+
+    Ray(const Vec3f& orig, const Vec3f& dir) : origin(orig), direction(normalize(dir)) {}
+
+    //we can use this as: Ray ray = Ray::cameraToPixel(camera, pixel_x, pixel_y);
+    //The cameraToPixel method is static because it creates a Ray without needing an instance of the Ray class. 
+    //It uses the camera's parameters and pixel coordinates to calculate the origin and direction of the ray.
+
+    static Ray cameraToPixel(const Camera &camera, int pixel_x, int pixel_y) {
+        float l = camera.near_plane.x ;
+        float r = camera.near_plane.y;
+        float b =camera.near_plane.z;
+        float t = camera.near_plane.w;
+        Vec3f v = normalize(camera.up);
+        Vec3f w = normalize((camera.gaze)*(-1.0));
+        Vec3f u = normalize(cross(v,w));
+        Vec3f m = camera.position + (camera.gaze)*camera.near_distance ;
+        float s_u = (pixel_x + 0.5)* (r-l) / camera.image_width;
+        float s_v = (pixel_y + 0.5)* (t-b) / camera.image_height;
+        Vec3f q = m + u*l + v*t ;
+        Vec3f s = q + u*s_u - v*s_v ;
+        return Ray(camera.position, normalize(s - camera.position));
+    }
+    struct HitRecord {
+        bool is_intersected;
+        float t;
+        Vec3f intersection_point;
+        Material material;
+        Vec3f normal;
+    };
+
+    HitRecord intersectionSphere(Sphere const &sphere) {
+
+    }
+
+    HitRecord intersectionTriangle(Triangle const&triange){
+
+    }
+
+    HitRecord intersectionMesh(Mesh const&mesh){
+
+    }
+
+    //compute_color(Ray r, int depth) içinde,, o rayi scenedeki tüm objelerle kesiştirmeye çalışacağız 
+    //(all spheres loop, all triangles loop,...) hit record tutacağız bir yandan ve bu kesişimin hitrecord.is_intersected==true ise
+    // ve bir önceki kesişimden daha yakınsa(Hitrecord.t < current_min_t) hit recordu güncelle.
+    //tüm objeleri döndüğünde elinde kalan hitrecord en yakın hit olmuş olacak.
+    //daha sonra bu ray'i ve hitrecordunu shading fonskiyonuna gönder. shading depthi artırarar compute_color'u geri çağırabilir.
+
+    //mainin içinde, cameradan tüm pixelleri dön, cameratopixel'i çağır, rayi elde et. Depthi 0 vererek compute_color'u her ray için çağır.
+    //Compute color bir renk dönecek, onu da 0,255 arasına sıkıştır. SON.
+};
 
 int main(int argc, char* argv[])
 {
